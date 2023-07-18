24906ae96347996714dbd7071b204053eab94380
Wolf of Wall Street <a1111@Finance.local>
2023-07-18 22:14:46 +0300

    first commit

---
+++ server/seeds/data/admins/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable max-len */
+const md5 = require('md5');
+
+module.exports = {
+    login: '11111111',
+    password: md5('11111111'),
+    email: 'admfinstandart@gmail.com',
+    id: 'admin_id'
+};
---
+++ src/apps/admin/services/messageWebsocket.js
@@ -0,0 +1,50 @@
+import io from 'socket.io-client';
+
+import { TOKEN_LOCAL_STORAGE_NAME } from '../constants/constants';
+import EventEmitter from 'eventemitter3';
+import { WS_MESSAGES_PORT } from '../../../../server/constants/constants';
+
+const WEBSOCKET_URL = process.env.NODE_ENV === 'production' ? `wss://https://trade.fin-standart.ru/:${WS_MESSAGES_PORT}` : `ws://localhost:${WS_MESSAGES_PORT}`;
+
+class MessageWebsocketController {
+    events = new EventEmitter();
+
+    connect () {
+        const socket = io(WEBSOCKET_URL, { transports: ['websocket'] });
+
+        this.socket = socket;
+
+        socket.on('connect', () => {
+            const token = localStorage.getItem(TOKEN_LOCAL_STORAGE_NAME);
+
+            socket.emit('token', {
+                type: 'admin',
+                token
+            });
+        });
+
+        socket.on('message', data => {
+            this.events.emit('message', data);
+        });
+    }
+
+    disconnect () {
+        this.socket && this.socket.disconnect();
+        this.socket = null;
+    }
+
+    sendMessage ({ receiverId, text }) {
+        if (!this.socket) {
+            return;
+        }
+
+        this.socket.emit('message', {
+            receiverId,
+            text
+        });
+    }
+}
+
+const messageWebsocketController = new MessageWebsocketController();
+
+export default messageWebsocketController;
